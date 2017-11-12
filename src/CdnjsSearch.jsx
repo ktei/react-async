165821a816823b75aa228754a7dadc09ffad9170
Rui Cheng <ktei2008@gmail.com>
2017-11-12 22:12:22 +1100

    cdnjs

---
+++ src/CdnjsSearch.jsx
@@ -0,0 +1,58 @@
+import axios from 'axios';
+import React from 'react';
+
+export default class CdnjsSearch extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      result: {},
+      isLoading: false,
+      query: 'jquery',
+    };
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  loadData() {
+    this.setState({ isLoading: true });
+    const apiUrl = this.constructApiUrl();
+    axios.get(apiUrl)
+      .then((response) => {
+        this.setState({ isLoading: false, result: response.data });
+      });
+  }
+
+  constructApiUrl() {
+    const query = this.state.query.trim();
+    if (!query) {
+      return '';
+    }
+    return `https://api.cdnjs.com/libraries/${encodeURIComponent(query)}`;
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <div className="container" style={{ margin: '15px auto' }}>
+          <h3>Loading...</h3>
+        </div>
+      );
+    }
+
+    const { name, filename, version, license, assets = [] } = this.state.result;
+    return (
+      <div className="container" style={{ margin: '15px auto' }}>
+        <label>Name</label> {name} <br />
+        <label>File name</label> {filename} <br />
+        <label>Version</label> {version} <br />
+        <label>License</label> {license} <br />
+        <label>All versions</label>
+        <div>
+          {assets.map(x => (<div>{x.version}</div>))}
+        </div>
+      </div>
+    );
+  }
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import ParisWeather from './ParisWeather';
+// import ParisWeather from './ParisWeather';
+import CdnjsSearch from './CdnjsSearch';
 
-ReactDOM.render(<ParisWeather />, document.getElementById('root'));
+ReactDOM.render(<CdnjsSearch />, document.getElementById('root'));
